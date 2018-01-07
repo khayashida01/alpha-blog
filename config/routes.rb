ab68b9ff37e57411c5d10eb752048bda1021dc5d
babysteps <babysteps.kh@gmail.com>
2018-01-07 21:34:08 +0000

    add edit user profile

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
   post 'users' => 'users#create'
+  resources :users
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
