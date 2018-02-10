5bc2906fe04ee5ad67ad6b4d2bdff630790a81b3
babysteps <babysteps.kh@gmail.com>
2018-02-10 16:32:20 +0000

    commit.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
+    @article.user = current_user
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
